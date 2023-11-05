8a46b51b4102002257fc09a66d3790334afbad9a
Ohiduzzaman Siam <mdsiamss10@gmail.com>
2023-11-05 12:48:39 +0600

    Update firebase.config.ts

--- firebase.config.ts
+++ firebase.config.ts
@@ -7,13 +7,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDRuJFwnhnLxJRqHFk4wPyZd6KUplp1QaY",
-  authDomain: "sichat69.firebaseapp.com",
-  projectId: "sichat69",
-  storageBucket: "sichat69.appspot.com",
-  messagingSenderId: "284150239936",
-  appId: "1:284150239936:web:7827bc9ce99e9c44121325",
+  apiKey: "AIzaSyCt7N38OqcFsYBoBgk7sToGmXgf4qkiybY",
+  authDomain: "demopro-6e9fe.firebaseapp.com",
+  projectId: "demopro-6e9fe",
+  storageBucket: "demopro-6e9fe.appspot.com",
+  messagingSenderId: "957566964529",
+  appId: "1:957566964529:web:b9d054a1e6ccc4d35d1610"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
